6975ab55db147050b9eca5a75aae9548cfc4f70f
bburnett86 <breton.burnett@gmail.com>
2017-03-28 16:32:31 -0500

    Create Home and Main components, create routes and default

--- app/index.js
+++ app/index.js
@@ -1,51 +1,5 @@
-var USER_DATA ={
-  name: 'Breton Burnett',
-  username: 'bburnett86',
-  image: 'https://robohash.org/autdebitisaccusantium.png?size=300x300&set=set1'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var ProfilePic = React.createClass({
-  render: function(){
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function(){
-    return(
-      <div>
-        <a href ={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function(){
-    return(
-      <div>{this.props.name}</div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function(){
-    return(
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-})
-
-ReactDOM.render(
-  <Avatar user={USER_DATA} />,
-  document.getElementById('app')
-);
+ReactDOM.render(routes, document.getElementById('app'));
